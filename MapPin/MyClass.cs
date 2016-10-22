25ca39dfb6e88a7fb84c3a9715d86453a4dc04d6
Harold Heim <harold.heim@telecomnancy.eu>
2016-10-22 13:20:53 +0200

    Pin map, iOS:OK, Android:notOK

---
+++ MapPin/MyClass.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace MapPin
+{
+	public class MyClass
+	{
+		public MyClass()
+		{
+		}
+	}
+}
---
+++ iOS/ViewController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+using UIKit;
+using Mapbox;
+using System.Linq;
+using CoreLocation;
+using Foundation;
+
+namespace MapBoxSampleiOS
+{
+    public partial class ViewController : UIViewController, IMapViewDelegate
+    {
+        public ViewController (IntPtr handle) : base (handle)
+        {
+        }
+
+        MapView mapView;
+
+		public override void ViewDidLoad()
+		{
+			base.ViewDidLoad();
+
+			// Create a MapView and set the coordinates/zoom
+			mapView = new MapView(View.Bounds);
+			mapView.SetCenterCoordinate(new CLLocationCoordinate2D(48.665, 6.1603), 11, false);
+
+			mapView.AddAnnotation(new PointAnnotation
+			{
+				Coordinate = new CLLocationCoordinate2D(48.665, 6.1603),
+				Title = "Marqueur test",
+				Subtitle = "Texte test"
+			});
+
+			// Set ourselves as the delegate
+			mapView.Delegate = this;
+
+			View.AddSubview(mapView);
+
+		}
+
+        public override void DidReceiveMemoryWarning ()
+        {
+            base.DidReceiveMemoryWarning ();
+
+            // Cleanup anything possible
+            mapView.EmptyMemoryCache ();
+        }
+    }
+}
+
