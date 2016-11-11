1d4d3f95683a4409570d6b87aadba99b022faaa2
Harold Heim <extrarold@gmail.com>
2016-11-11 21:12:35 +0100

    implémentation Mvvmlight

---
+++ Maps/Maps/ViewModel/MainViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.Command;
+
+namespace Maps.ViewModel
+{
+    public class MainViewModel : ViewModelBase
+    {
+
+
+    }
+}
