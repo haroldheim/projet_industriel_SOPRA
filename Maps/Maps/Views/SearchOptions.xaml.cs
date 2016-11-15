1b6130ac5c6c70967dab303d0537d9ab473ee1ef
Harold Heim <harold.heim@telecomnancy.eu>
2016-11-15 17:30:25 +0100

    ajout écran SearchOptions

---
+++ Maps/Maps/Views/SearchOptions.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+
+namespace Maps
+{
+	public partial class SearchOptions : ContentPage
+	{
+		public SearchOptions()
+		{
+
+			InitializeComponent();
+
+			Content = new TableView
+			{
+				Intent = TableIntent.Form,
+				Root = new TableRoot("")
+				{
+					new TableSection ("What kind of home are you searching for")
+					{
+						new TextCell
+						{
+							Text = "TextCell Text",
+							 Detail = "TextCell Detail"
+						},
+						new EntryCell {
+							Label = "EntryCell:",
+							Placeholder = "default keyboard",
+							Keyboard = Keyboard.Default
+						}
+					},
+					new TableSection ("Surface Area") {
+						new EntryCell {
+							Label = "Another EntryCell:",
+							Placeholder = "phone keyboard",
+							Keyboard = Keyboard.Telephone
+						},
+						new SwitchCell {
+							Text = "SwitchCell:"
+						}
+					}
+				}
+			};
+		}
+	}
+}
---
+++ Maps/Maps/Views/test.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace Maps
+{
+	public partial class test : ContentPage
+	{
+		public test()
+		{
+			InitializeComponent();
+		}
+	}
+}
