e268f6674309b6930143da1bd7877a6882f3ef35
ClemenceMoulin <clemence.moulin@telecomnancy.net>
2017-01-05 15:05:50 +0100

    splash screen, logo, nom appli : OK pour Android

--- Maps/Maps.Droid/MainActivity.cs
+++ Maps/Maps.Droid/MainActivity.cs
@@ -9,7 +9,7 @@ using Android.OS;
 
 namespace Maps.Droid
 {
-    [Activity(Label = "FindMyHome", Icon = "@drawable/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    [Activity(Label = "FindMyHome", Icon = "@drawable/icon", Theme = "@style/MainTheme", ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle bundle)
---
+++ Maps/Maps.Droid/SplashActivity.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+using Android.App;
+using Android.Content.PM;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+
+namespace Maps.Droid
+{
+    [Activity(Label = "FindMyHome", Icon = "@drawable/icon", ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation, Theme = "@style/Theme.Splash", //Indicates the theme to use for this activity
+             MainLauncher = true, //Set it as boot activity
+             NoHistory = true)] //Doesn't place it in back stack
+    public class SplashActivity : Activity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+            //System.Threading.Thread.Sleep(3000); //Let's wait awhile...
+            this.StartActivity(typeof(MainActivity));
+        }
+    }
+}
+
